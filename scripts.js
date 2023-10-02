54496a155961cbf0c1957ce516daab2eb53c8999
Ana Julia Gari  Albano <112116683+n4ju15@users.noreply.github.com>
2023-10-02 16:42:15 -0300

    Feat: create function map all items with map

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
+const buttonMapAll = document.querySelector('.map-all')
 let myLi = ''
 
+// Function para mostrar todos os cards
+function showAll(productsArray) {
+    myLi = '' // Reinicia a variável myLi para evitar acumulação de cards
 
-function showAll() {
-    myLi = '' // Reinicie a variável myLi para evitar acumulação de cards
-
-    menuOptions.forEach(product => {
+    productsArray.forEach(product => {
         myLi += `
             <li>
             <img src=${product.src}>
@@ -19,4 +20,18 @@ function showAll() {
     list.innerHTML = myLi
 }
 
-buttonShowAll.addEventListener('click', showAll)
+
+// Function para mapear todos os itens e dar 10% de desconto em todos os itens
+function mapAllItems() {
+    const newPrices = menuOptions.map( (product) => ({
+        ...product,     // Spread Operator
+        price: product.price * 0.9,
+    }))
+
+    showAll(newPrices)
+}
+
+
+
+buttonShowAll.addEventListener('click', () => showAll(menuOptions))
+buttonMapAll.addEventListener('click', mapAllItems)
